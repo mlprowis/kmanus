e992559eed44f8185a601a0d23f273dda9e4ad44
mlprowis <mlprowis@gmail.com>
2026-05-30 13:12:05 +0200

    Update start.sh to execute shell script
    Replaced node command with a shell script execution.

--- start.sh
+++ start.sh
@@ -1,3 +1,1 @@
-#!/bin/sh
-node -e "const fs=require('fs');fs.mkdirSync('/etc/grvt-grid',{recursive:true});fs.writeFileSync('/etc/grvt-grid/master.key',Buffer.from(process.env.MASTER_KEY_HEX,'hex'));"
-exec node packages/bot/dist/dashboard/server.js
+sh /app/start.sh
